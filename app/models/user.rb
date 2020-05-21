f8d8bf1170de52b12d933509a75d1206ba083e30
smriti154 <smriti.gupta@joshsoftware.com>
2020-05-21 23:36:34 +0530

    Fixed rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_secure_password
-  validates :username, presence: true, uniqueness: true
-  validates :password, presence: true, length: {in: 4..15}
 
-  enum roles: {shop_owner: 0}
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 4..15 }
+
+  enum roles: { shop_owner: 1 }
 
   def role
     User.roles.key(role_id)
   end
 
   def shop_owner?
-    role == "shop_owner"
+    role == 'shop_owner'
   end
 end
