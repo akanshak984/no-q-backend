301d06ee8d5840b92a9818dabf484e396a5e8d0b
akanshak984 <akanshak984@gmail.com>
2020-05-21 22:41:21 +0530

    role_id field added

--- db/migrate/20200521155155_create_users.rb
+++ db/migrate/20200521155155_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.integer :password_digest
+      t.integer :role_id
       t.timestamps
     end
   end
