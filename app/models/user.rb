98d77a30f9c252691865da864e6652d9f4a2a821
smriti154 <smriti.gupta@joshsoftware.com>
2020-05-22 00:09:43 +0530

    Added role enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 4..15 }
 
-  enum roles: { shop_owner: 1 }
-
-  def role
-    User.roles.key(role_id)
-  end
-
-  def shop_owner?
-    role == 'shop_owner'
-  end
+  enum role: { shop_owner: 1 }
 end
