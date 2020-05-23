98e41f85db7002942ac75eba44cb18996d1d459d
smriti154 <smriti.gupta@joshsoftware.com>
2020-05-23 13:31:27 +0530

    Added serializer

--- app/serializers/store_serializer.rb
+++ app/serializers/store_serializer.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class StoreSerializer < ActiveModel::Serializer
-  attributes :id, :name, :code, :address, :city, :state, :pincode,
+  attributes :id, :name, :code, :address, :city,
+             :state, :pincode,
              :opening_time, :closing_time,
              :duration, :capacity, :available_days,
-             :deleted_at, :proposed_slots,
-             :categories
+             :deleted_at, :slots, :categories
 
   has_many :categories
+  has_many :slots
 
   def opening_time
     object.opening_time.strftime('%H:%M')
@@ -16,6 +17,4 @@ class StoreSerializer < ActiveModel::Serializer
   def closing_time
     object.closing_time.strftime('%H:%M')
   end
-
-  delegate :proposed_slots, to: :object
 end
